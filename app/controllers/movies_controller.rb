1ba5f75ca26fdf830e4c6123783cbb964ba255ad
Victoria <missvictoriachen@gmail.com>
2013-09-02 19:37:17 -0700

    Added movie rating filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,20 +2,43 @@ class MoviesController < ApplicationController
 
   SELECTED_CLASS = "hilite"
   
-def show
+  def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = params[:sort] != nil && Movie.column_names.include?(params[:sort]) ?
-	      Movie.order(params[:sort]) :
-	      Movie.all
+    populate_ratings
+    populate_movies
 
     # update css class for sorted column
-    @title_css = SELECTED_CLASS if params[:sort] == "title"
-    @release_date_css = SELECTED_CLASS if params[:sort] == "release_date"
+    @title_css = SELECTED_CLASS if session[:movies_sort] == "title"
+    @release_date_css = SELECTED_CLASS if session[:movies_sort] == "release_date"
+  end
+
+  def populate_ratings
+    @all_ratings = Movie.all_ratings
+
+    if (params["ratings"] != nil && params["ratings"].count > 0)
+      @checked_ratings = params["ratings"].keys
+      session[:checked_ratings] = @checked_ratings
+    elsif (session[:checked_ratings] != nil)
+      @checked_ratings = session[:checked_ratings]
+    else
+      @checked_ratings = @all_ratings
+    end
+  end
+
+  def populate_movies
+    if (params[:sort] != nil && Movie.column_names.include?(params[:sort]))
+      session[:movies_sort] = params[:sort]
+      @movies = Movie.where("rating" => @checked_ratings).order(params[:sort])
+    elsif (session[:movies_sort] != nil)
+      @movies = Movie.where("rating" => @checked_ratings).order(session[:movies_sort])
+    else
+      @movies = Movie.where("rating" => @checked_ratings)
+    end
   end
 
   def new
